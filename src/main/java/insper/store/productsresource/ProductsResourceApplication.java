121a9dd368a71bf3825c765aff306746a93f02f9
gustavolp1 <glpachecocsa@gmail.com>
2024-04-08 12:57:49 -0300

    initializr

---
+++ src/main/java/insper/store/productsresource/ProductsResourceApplication.java
@@ -0,0 +1,13 @@
+package insper.store.productsresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsResourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/insper/store/productsresource/ProductsResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package insper.store.productsresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
