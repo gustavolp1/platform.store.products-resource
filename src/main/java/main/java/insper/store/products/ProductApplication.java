a8884d932031be76dc7501937cb6ce77c7b4cd39
gustavolp1 <glpachecocsa@gmail.com>
2024-04-08 23:43:04 -0300

    swagger2

--- src/main/java/main/java/insper/store/products/ProductApplication.java
+++ src/main/java/main/java/insper/store/products/ProductApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableSwagger2
 @SpringBootApplication
 public class ProductApplication {
 
