c5d764657eea310fa64f4ab6ac8ab4059f80a598
alemagno10 <alemagno03@gmail.com>
2024-04-10 13:08:44 -0300

    remove: requestmapping

--- src/main/java/main/java/insper/store/products/ProductResource.java
+++ src/main/java/main/java/insper/store/products/ProductResource.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -17,7 +17,6 @@ import insper.store.products.ProductOut;
 import insper.store.products.ProductsController;
 
 @RestController
-@RequestMapping("/api")
 public class ProductResource implements ProductsController {
 
     @Autowired
